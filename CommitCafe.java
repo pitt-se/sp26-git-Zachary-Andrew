13b35165cf27bc790236990956f9a136dad7cb08
Zachary Andrew <zacha3905@gmail.com>
2026-04-06 13:37:52 -0400

    Feature: Implimented brew

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,17 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
-    // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String barista = "Jason"; //Change name
+    //Add a nickname or title for the barista
+    private static String nickname = "Jace";
 
     private static int cups = 0;
 
     public static void brew(String drink) {
-        // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
-        // STUDENT_TODO_2B: Add a second brew-related improvement
+        // Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println("Making " + drink);
+        // Add a second brew-related improvement
+        System.out.println(nickname + " has finished making your dirnk");
     }
 
     public static void printSummary() {
